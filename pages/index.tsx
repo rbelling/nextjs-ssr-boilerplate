ef73da25dbc1b8cfbd657b7490b0d1be15cb2e89
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2019-06-21 18:15:51 -0400

    styled-jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 const getName = (): string => "42";
 
 const Index = (): any => <div>
+    <style jsx>{`
+      p {
+        color: red;
+      }
+    `}</style>
     <p>Hello {getName()}!!!</p>
 </div>;
 export default Index;
