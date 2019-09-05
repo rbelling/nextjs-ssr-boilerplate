306f98b0197176b03ba09bdefa0c23eb3cfa2683
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2019-09-05 08:05:04 -0400

    chore: add right return type

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import style from "../styles/index.scss";
 const getName = (): string => "Visitor";
 
 const Index = (): object => (
+const Index = (): React.FC => (
   <div className={style.heroBlock} onClick={() => window.alert(getName())}>
     <h1 className={style.heading}>Hello {getName()}, this is next.js</h1>
   </div>
