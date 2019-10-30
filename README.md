f6e3f626a7e6bba168aeac79890cff3c2c508c0a
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2019-10-30 01:02:18 -0400

    wip: print text from markdown

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
-> Next.js boilerplate for universal apps, with TypeScript and SASS support
+> a quote
 
 # Upcoming features
-- [x] add typescript
-- [x] add css support, with autoprefixer and css-next
-- [x] add css modules
-- [x] add linting
-- [x] add tools listed in https://evilmartians.com/chronicles/autoprefixer-7-browserslist-2-released
-- [x] add dynamic imports https://nextjs.org/docs#basic-usage-also-does-ssr
-- [x] add webpack-bundle-analyzer
-- [ ] test routes (server / clientside)
-- [ ] add offline support
-- [ ] save as bp
-- [ ] add travis bundle-checker
-- [ ] add light wallet on CI?
+- [ ] an item 
+- [x] another item
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 // @ts-ignore
@@ -7,11 +8,14 @@ import style from "../styles/index.scss";
 const DynamicComponentWithNoSSR = dynamic(() => import("../components/dynamic"), {
   ssr: false
 });
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const input = require("../markdown/README.md");
 
 const Index: React.FC = () => (
   <div className={style.heroBlock}>
     <h1 className={style.heading}>Hello human, this is SSR.</h1>
     <DynamicComponentWithNoSSR />
+    <ReactMarkdown source={input} escapeHtml={false} />
     <Link href="/about">
       <a>Click to visit another page</a>
     </Link>
